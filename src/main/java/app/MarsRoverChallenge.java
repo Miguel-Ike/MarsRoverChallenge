d846972d78f941118ea656c711857a7f46e0e3bf
Miguel Angel Martinez Rodriguez <miguel_angel-m@outlook.com>
2024-01-09 13:31:08 -0400

    Initial commit

---
+++ src/main/java/app/MarsRoverChallenge.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app;
+
+/**
+ *
+ * @author Miguel_Angel
+ */
+public class MarsRoverChallenge {
+
+    private static void init() {
+        String msg = """
+                |--------------------------------------------------------------------------|
+                |            Start of the Mars Rover challenge by Miguel-Ike...            |
+                |--------------------------------------------------------------------------|
+                """;
+        System.out.println("\n" + msg);
+    }
+
+    private static void end() {
+        String msg = """
+                |--------------------------------------------------------------------------|
+                |             End of the Mars Rover challenge by Miguel-Ike...             |
+                |--------------------------------------------------------------------------|
+                """;
+        System.out.println("\n" + msg);
+    }
+
+    public static void main(String[] args) {
+        init();
+
+        end();
+    }
+}
